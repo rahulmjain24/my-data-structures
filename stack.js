9b632f74244baad646b657e20b42780c30cfc628
Rahul Jain <rahul.jain.22.1@mountblue.tech>
2022-12-08 11:52:15 +0530

    Create stack

---
+++ stack.js
@@ -0,0 +1,51 @@
+class Node {
+    constructor(value) {
+       this.value = value
+        this.next = null;
+    }
+}
+
+class Stack {
+    constructor() {
+        this.first = null
+        this.last = null
+        this.size = 0
+    }
+
+    push(val) {
+        const node = new Node(val)
+        if(this.first == null) {
+            this.first = node
+            this.last = node
+        } else {
+            const temp = this.first
+            this.first = node
+            this.first.next = temp
+        }
+        return ++this.size
+    }
+
+    pop() {
+        if(this.size === 0) {
+            return null
+        }
+        const current = this.first
+        this.first = this.first.next
+        if(this.first === null) {
+            this.last = null
+        } else {
+            current.next = null;
+        }
+        this.size--
+        return current.value
+    }
+}
+
+const stack = new Stack()
+stack.push(1)
+stack.push(2)
+stack.push(3)
+
+stack.pop()
+
+console.log(stack.pop())
